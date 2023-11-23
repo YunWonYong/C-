af3d5e88b7f97f64ede2e5604b1bd14d5f8244ad
wonyong.yun <wonyong.yun@playlinks.com>
2023-11-23 14:34:57 +0900

    chapter2 console

---
+++ PopeKim/chapter2/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        Console.WriteLine("WriteLine을 쓰면");
+        Console.WriteLine("한 줄씩 나옵니다.");
+        Console.Write("Write는 아닙니다.");
+        Console.Write("이어져서 나오죠?");
+        Console.WriteLine("Write 뒤에 WriteLine을 썻습니다.");
+        
+        Console.Write("바구니 안에 담긴 사과의 개수: ");
+        Console.WriteLine(12);
+        Console.Write("사과 바구니의 무게: ");
+        Console.WriteLine(1.32);
+
+        Console.WriteLine("!!?@#!$");
+        Console.WriteLine("큰따옴표 \" 출력");
+        // 2-1
+        Console.WriteLine("Hello C#");
+        Console.Write("10+10 =");
+        Console.WriteLine(20);
+        Console.WriteLine("C#의 세계에 오신 것을 환영합니다.");
+        Console.WriteLine("감사합니다.");
+
+        // 2-3
+        Console.Write("출력");
+        Console.Write("문");
+        Console.Write("\" ");
+        Console.Write("기본");
+        Console.WriteLine("문제");
+
+        // 심화 2-1 
+        Console.Write("무엇이 무엇이");
+        Console.WriteLine("문제일까?");
+        Console.WriteLine("\"");
+
+        // 심화 2-2
+        Console.WriteLine("?!\"");
+        Console.Write("\"\"\"\"\"\"");
+    }
+}
